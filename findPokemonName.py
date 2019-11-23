1c57f324d8778156f35250e74d56befab213de05
AbhishekS-2099 <abhishek.sankar.in@gmail.com>
2019-11-23 22:36:39 +0530

    Testing if difflib.get_close_matches is really the solution, since it doesn't seem to do the job

--- findPokemonName.py
+++ findPokemonName.py
@@ -3,9 +3,18 @@ import json
 
 # pokemonNamesListJSON = json.load('pokemonNames.json')
 # print(pokemonNamesListJSON)
-
+pokemonNameJSONconvertedToList = []
 with open('pokemonNames.json','r') as pokemonNamesList:
 	pokemonData = json.load(pokemonNamesList)
-	print(pokemonData)
-# 	for name in pokemonNamesList['pokemon']:
-# 		print(name)
+	# print(pokemonData['pokemon'])
+for name in pokemonData['pokemon']:
+	pokemonNameJSONconvertedToList.append(name)
+print(pokemonNameJSONconvertedToList)
+# for name in pokemonNameJSONconvertedToList:
+# 	print(name)
+def findClosestPokemon(listOfPokemonNames,wordToFindClosestPokemonName):
+	print(get_close_matches(wordToFindClosestPokemonName,listOfPokemonNames,cutoff=0))
+
+# if __name__ == "__main__":
+# 	nameGivenByUser = input("Enter any word and we'll try to find the closest pokemon name to that word\n")
+# 	findClosestPokemon(nameGivenByUser,pokemonNameJSONconvertedToList)
